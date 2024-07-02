69b9f6c2fd3b8f88eb826f6e4cbd4dcdf0f741ed
Anne Mitchell <annegorithm@gmail.com>
2024-07-02 16:40:07 -0400

    class edits

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,19 @@
-# Add the 2 requires you will need here
+require 'rspec'
+require './lib/student'
 
 
 describe Student do
   describe '#initialize' do
-    it 'is an instance of student' do
-      # write the code to initialize a new student object
+      it 'is an instance of student' do
+        # write the code to initialize a new student object
+        student = Student.new(Anne)
+        expect(student).to be_a student
+      end
+      @name = name
+      # test it has cookies
+      # test it can add cookies
+  
+  
+  
     end
-    # test it has a name
-    # test it has cookies
-    # test it can add cookies
-  end
 end
